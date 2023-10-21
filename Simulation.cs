17c48b6c74f4e7188dbe39284c0e0ef8baf0d32a
xGeorg315 <georg9675@gmail.com>
2023-10-21 16:02:56 +0200

    Code Maintanances

--- Simulation.cs
+++ Simulation.cs
@@ -7,10 +7,10 @@ class Simulation
     public void AnzahlHändler()
     {
         Console.WriteLine("Wieviele Zwischenhändler nehmen teil?");   
-        while(true)                                                                             //Wartet bis eine Zahl eingegeben wurde
+        while(true)                                                                                     //Wartet bis eine Zahl eingegeben wurde
         {                                                                        
             string UserInput = Console.ReadLine()!;
-            if (Int32.TryParse(UserInput, out AnzahlZwischenhändler)) break;                    //Check ob UserInput eine Zahl ist 
+            if (Int32.TryParse(UserInput, out AnzahlZwischenhändler)) break;                            //Check ob UserInput eine Zahl ist 
             Console.WriteLine("Sieht so aus als wäre das keine Nummer gewesen");
             Console.WriteLine("Erneut Versuchen:");
         }
@@ -18,45 +18,45 @@ class Simulation
 
     public void ErstelleHändler()
     {
-        for(int i = 1; i <= AnzahlZwischenhändler; i++)                                         //Erstelle x Zwischenhändler
+        for(int i = 1; i <= AnzahlZwischenhändler; i++)                                                 //Erstelle x Zwischenhändler
         {                                   
             Zwischenhändler Zwischenhändler = new Zwischenhändler();
 
             while(true)
             {
-                string Ausgabe = "Name von Zwischenhändler " + i;                                   //Frage Name des Händlers ab
+                string Ausgabe = "Name von Zwischenhändler " + i;                                       //Frage Name des Händlers ab
                 Console.WriteLine(Ausgabe);
                 string NameHändler = Console.ReadLine()!;
 
-                Ausgabe = "Firma von " + NameHändler;                                              //Frage Firma des Händlers ab
+                Ausgabe = "Firma von " + NameHändler;                                                   //Frage Firma des Händlers ab
                 Console.WriteLine(Ausgabe);
                 string NameFirma = Console.ReadLine()!;   
                 
-                if(NameFirma != null && NameHändler != null)
+                if(!string.IsNullOrWhiteSpace(NameFirma) && !string.IsNullOrWhiteSpace(NameHändler))
                 {
-                    Zwischenhändler.Name = NameHändler;                                                //Speichere Name in den Händler
-                    Zwischenhändler.Firma = NameFirma;                                               //Speichere Firma in den Händler
+                    Zwischenhändler.Name = NameHändler;                                                 //Speichere Name in den Händler
+                    Zwischenhändler.Firma = NameFirma;                                                  //Speichere Firma in den Händler
                     break;
                 }
                 Console.WriteLine("Einer der beiden Namen wurde nicht korrekt ausgefüllt\r");
                 Console.WriteLine("Versuche es nochmal");
             }
-            Globals.Händler.Add(Zwischenhändler);                                               //Speichere die Händler in eine Globale var
+            Globals.Händler.Add(Zwischenhändler);                                                       //Speichere die Händler in eine Globale var
         }
     }
 
     public void StarteSimulation()
     {
-        int Tag = 1;
+        int AktuellerTag = 1;
         while (true)
         {                                                                                                            
-            foreach (Zwischenhändler Händler in Globals.Händler)                                //Endlosschleife für die Simulation 
+            foreach (Zwischenhändler Händler in Globals.Händler)                                       //Endlosschleife für die Simulation 
             {                               
-                string Output = Händler.Name + " von " + Händler.Firma + " | Tag " + Tag;       //Erstelle Output String
+                string Output = Händler.Name + " von " + Händler.Firma + " | Tag " + AktuellerTag;              //Erstelle Output String
                 Console.WriteLine(Output);
-                CheckChoices();                                                                 //Checke die Möglichkeiten des Händlers
+                CheckChoices();                                                                        //Checke die Möglichkeiten des Händlers
             }
-            Tag++;
+            AktuellerTag++;
         }
     }
 
@@ -64,7 +64,7 @@ class Simulation
     {
         Console.WriteLine("b) Runde beenden");   
         while (true)
-        {                                                                       //Warte auf gültigen Input
+        {                                                                                               //Warte auf gültigen Input
             string input = Console.ReadLine()!;
             if (input == "b") break;
             Console.WriteLine("Ungültige Eingabe :-(");
