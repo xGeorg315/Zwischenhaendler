06b246e8e4c501f8b3067b00621e7d1ff7e15ece
xGeorg315 <georg9675@gmail.com>
2023-10-26 10:14:37 +0200

    Code Kommentierung

--- DateiLesen.cs
+++ DateiLesen.cs
@@ -7,6 +7,9 @@ class DateiLesen
     string PfadGesuchterDatei = "";
     string [] DateiInhalt;
 
+    /// <summary>
+    /// Starte Lese Zyklus
+    /// </summary>
     public void LeseProdukte()
     {   
         FindeDatei(GesuchteDatei);
@@ -14,49 +17,66 @@ class DateiLesen
         FilterProdukte();
     }
 
+    /// <summary>
+    /// Suche die Datei
+    /// Funktion such im Strang alles ab, geht jedoch nicht andere Ordner/Stränge rein
+    /// </summary>
     public void FindeDatei(string GesuchteDatei)
     {
         string? AktuellerPfad = Directory.GetCurrentDirectory();
         string [] DateiSuche;
         bool Gefunden = false;
 
+        //Warte bis die Datei gefunden wurde
         while(!Gefunden)
-        {
+        {   
+            //Wenn der Pfad zuende ist breche ab
              if(AktuellerPfad == null)
             {
                 Console.WriteLine("Die Produkt Datei konnte nicht gefunden werden");
                 break;
             }
-
+            //Liste die Dateien im aktuellen Pfad
             DateiSuche = Directory.GetFiles(AktuellerPfad);
-           
+            // Checke ob gefundene Datei darunter ist
             foreach(var Datei in DateiSuche)
             {
+                //Speichere Pfad der Gesuchten datei wenn Datei vorhanden
                 if(Datei.Contains(GesuchteDatei))
                 {
                     Gefunden = true;
                     PfadGesuchterDatei = Datei;
                 }
             }
+            //Gehe einen Ordner zurück
             AktuellerPfad = Path.GetDirectoryName(AktuellerPfad);
         }
     }
-
+    /// <summary>
+    /// Lese die Datei aus
+    /// </summary>
     public void LeseDatei()
     {
+        //Checke ob Datei noch existiert
         if(!File.Exists(PfadGesuchterDatei)){
             Console.WriteLine("Beim Lesen ist etwas schief gelaufen"); 
+            return;
         }
-        
+        //Lese Datei Zeile für Zeile aus
         DateiInhalt = File.ReadAllLines(PfadGesuchterDatei);
-    }    
-
+    } 
+       
+    /// <summary>
+    /// Speichere die einzelnen Produkte in getrennte Objekte
+    /// Eine saubere und einheitliche Struktur sowie Formatierung der Yaml Datei ist dafür notwendig
+    /// </summary>
     public void FilterProdukte()
     {
         Produkte Produkt = new Produkte(); 
-
+        //Gehe alle Zeilen durch
         foreach (var Zeile in DateiInhalt)
         {
+            //Speichere die einzelenen Attribute in das angelegte Objekt
             if(Zeile.Contains("- Name:"))
             {
               string name = Zeile.Replace("- Name: ", "");
@@ -77,6 +97,7 @@ class DateiLesen
               if (Int32.TryParse(Haltbarkeit, out IntBasispreis)){
                 Produkt.BasisPreis = IntBasispreis;
               }
+                //Speichere das Produkt in die Globale var und lege ein neues Produkt an
                 Globals.VerfügbareProdukte.Add(Produkt);
                 Produkt = new Produkte();
             }
